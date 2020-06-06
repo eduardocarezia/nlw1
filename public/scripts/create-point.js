9a9bdd77d56fcdb1e5621965674fd895996b6453
Eduardo <eduardocarezia@gmail.com>
2020-06-06 17:29:24 -0300

    added front-end

---
+++ public/scripts/create-point.js
@@ -0,0 +1,71 @@
+function populateUFs() {
+  const ufSelect = document.querySelector("select[name=uf]")
+
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then(res => res.json())
+    .then(states => {
+
+      for (state of states) {
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+      }
+
+    })
+}
+
+populateUFs()
+
+function getCities(event) {
+  const citySelect = document.querySelector("select[name=city]")
+  const stateInput = document.querySelector("select[name=state]")
+
+  const ufValue = event.target.value
+
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+  citySelect.innerHTML = '<option>Selecione a Cidade</option>'
+  citySelect.disabled = true
+  fetch(url)
+    .then(res => res.json())
+    .then(cities => {
+
+      for (const city of cities) {
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+      }
+      citySelect.disabled = false
+    })
+}
+
+document
+  .querySelector("select[name=uf]")
+  .addEventListener("change", getCities)
+
+// Itens de Coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectItem)
+}
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectItem(event) {
+  const itemLi = event.target
+  itemLi.classList.toggle("selected")
+
+  const itemId = itemLi.dataset.id
+
+  const alreadSelected = selectedItems.findIndex(item => item == itemId)
+
+  if (alreadSelected >= 0) {
+    const filtredItems = selectedItems.filter(item => {
+      const itemIsDiferrent = item != itemId
+      return itemIsDiferrent
+    })
+    selectedItems = filtredItems
+  } else {
+    selectedItems.push(itemId)
+  }
+  
+  collectedItems.value = selectedItems 
+}
